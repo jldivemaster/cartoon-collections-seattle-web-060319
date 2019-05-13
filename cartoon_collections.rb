2e7ef2a6a5e2c6cddfe452f2ed53515861e7c1e4
Jason Leach <jlspursfan@gmail.com>
2019-05-13 01:25:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,11 +9,11 @@ def summon_captain_planet(arr)
   arr.map{ |ele| ele.upcase + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any?{ |ele| ele.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  find.arr{ |ele| cheese_types.include?(ele)}
 end
