4bc1968b1ce18bb52f38cf80cb5d4488962ce85b
Jason Leach <jlspursfan@gmail.com>
2019-05-13 01:23:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def roll_call_dwarves(arr)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map{ |ele| ele.upcase + "!"}
 end
 
 def long_planeteer_calls# code an argument here
