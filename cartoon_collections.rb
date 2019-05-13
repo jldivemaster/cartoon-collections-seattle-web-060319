4e3574f8acea906cefff453b3d5eaba4edbcf80a
Jason Leach <jlspursfan@gmail.com>
2019-05-13 01:25:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  find.arr{ |ele| cheese_types.include?(ele)}
+  arr.find{ |ele| cheese_types.include?(ele)}
 end
